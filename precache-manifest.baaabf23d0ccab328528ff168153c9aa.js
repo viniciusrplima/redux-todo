353d6233d52437aa6731b07597c3339c06c90bd0
Vinicius Rodrigues <vinicius.rodrigues.lima@ccc.ufcg.edu.br>
2020-06-21 11:15:40 -0300

    Updates

---
+++ precache-manifest.baaabf23d0ccab328528ff168153c9aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee08a7d6480faffe26058775b82e5860",
+    "url": "/redux-todo/index.html"
+  },
+  {
+    "revision": "ed9341477edaf8ae98b5",
+    "url": "/redux-todo/static/css/main.fd9db2f9.chunk.css"
+  },
+  {
+    "revision": "6d35eb2a767bdbe95806",
+    "url": "/redux-todo/static/js/2.0109f38b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/redux-todo/static/js/2.0109f38b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed9341477edaf8ae98b5",
+    "url": "/redux-todo/static/js/main.dc647030.chunk.js"
+  },
+  {
+    "revision": "8618abb5854e7cd8976b",
+    "url": "/redux-todo/static/js/runtime-main.c333c30a.js"
+  }
+]);
